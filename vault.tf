61626a68d9df8e5a2bb2844cf4339e1e1da04bee
Mark West <mark.west@hashicorp.com>
2023-06-22 15:16:31 -0700

    Support for JWT & HCP Namespaces

--- vault.tf
+++ vault.tf
@@ -10,54 +10,115 @@ terraform {
 provider "vault" {
   # Configuration options
   address = hcp_vault_cluster.example.vault_public_endpoint_url
-  token = hcp_vault_cluster_admin_token.example.token
+  # token = hcp_vault_cluster_admin_token.example.token
   skip_child_token = true
   skip_get_vault_version = true
 }
 
-# resource "vault_auth_backend" "example" {
-#   namespace = "admin"
-#   type = "userpass"
+# https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/namespace#nested-namespaces
+variable "child_namespaces" {
+  type = set(string)
+  default = [
+    "child_0",
+    "child_1",
+    "child_2",
+  ]
+}
 
-#   tune {
-#     max_lease_ttl      = "25h"
-#     listing_visibility = "unauth"
-#   }
-# }
+resource "vault_namespace" "children" {
+  for_each  = var.child_namespaces
+  namespace = "admin"
+  path      = each.key
+}
 
-# resource "vault_mount" "kvv1" {
-#   path        = "secret"
-#   type        = "kv"
-#   options     = { version = "1" }
-#   description = "KV Version 1 secret engine mount"
-# }
+resource "vault_mount" "children" {
+  for_each  = vault_namespace.children
+  namespace = each.value.path_fq
+  path      = "secrets"
+  type      = "kv"
+  options = {
+    version = "1"
+  }
+}
 
-# resource "vault_kv_secret" "foo" {
-#   path = "${vault_mount.kvv1.path}/foo"
-#   data_json = jsonencode(
-#     {
-#     "data": "Wanna Get Away"
-#     }
-#   )
-# }
+resource "vault_generic_secret" "children" {
+  for_each  = vault_mount.children
+  namespace = each.value.namespace
+  path      = "${each.value.path}/secret"
+  data_json = jsonencode(
+    {
+      "ns" = each.key
+    }
+  )
+}
 
 
-# # There is a bug in the provider documentation i.e.:
-# # https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/identity_mfa_pingid#example-usage
-# resource "vault_identity_mfa_pingid" "example" {
-#   settings_file_base64 = base64encode(file("./../../../Downloads/pingid.properties"))
-# #   policies = [ vault_policy.example.name]
-# #   mount_accessor = vault_auth_backend.example.path
+# There is a bug in the provider documentation i.e.:
+# https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/identity_mfa_pingid#example-usage
+resource "vault_identity_mfa_pingid" "example" {
+  namespace = "admin"
+  settings_file_base64 = base64encode(file("./../../../Downloads/pingid.properties"))
+#   policies = [ vault_policy.example.name]
+#   mount_accessor = vault_auth_backend.example.path
 
-# }
+}
 
-# resource "vault_policy" "example" {
-#   name = "ping-policy"
+# https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/jwt_auth_backend#example-usage
+resource "vault_jwt_auth_backend" "example" {
+  for_each  = vault_mount.children
+  namespace = each.value.namespace
+    description         = "Demonstration of the Terraform JWT auth backend"
+    path                = "jwt"
+    oidc_discovery_url  = "https://myco.auth0.com/"
+    bound_issuer        = "https://myco.auth0.com/"
+}
 
-#   policy = <<EOT
-# path "secret/foo" {
-#   capabilities = ["read"]
-#   mfa_methods  = ["ping"]
-# }
-# EOT
-# }
+# https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/jwt_auth_backend_role#example-usage
+resource "vault_jwt_auth_backend_role" "example" {
+  for_each  = vault_mount.children
+  namespace = each.value.namespace
+  backend         = vault_jwt_auth_backend.example[each.key].path
+  role_name       = "test-role"
+  token_policies  = ["default", "dev", "prod"]
+
+  bound_audiences = ["https://myco.test"]
+  bound_claims = {
+    color = "red,green,blue"
+  }
+  user_claim      = "https://vault/user"
+  role_type       = "jwt"
+}
+
+# https://developer.hashicorp.com/vault/docs/enterprise/mfa/mfa-pingid#configuration
+resource "vault_policy" "example" {
+  for_each  = vault_mount.children
+  namespace = each.value.namespace
+  name = "ping-policy"
+
+  policy = <<EOT
+path "secret/foo" {
+  capabilities = ["read"]
+  mfa_methods  = ["ping"]
+}
+EOT
+}
+
+# https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/mount#example-usage
+resource "vault_mount" "kvv1" {
+  namespace = "admin"
+  path        = "secret"
+  type        = "kv"
+  options     = { version = "1" }
+  description = "KV Version 1 secret engine mount"
+}
+
+# https://developer.hashicorp.com/vault/docs/enterprise/mfa/mfa-pingid#configuration
+resource "vault_kv_secret" "foo" {
+  namespace = vault_mount.kvv1.namespace
+  path = "${vault_mount.kvv1.path}/foo"
+  data_json = jsonencode(
+    {
+    "data": "Wanna Get Away"
+    }
+  )
+}
